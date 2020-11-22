cc3ff6c242f2d6eb05604acb85f95e56ec1860ba
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2020-11-23 00:49:15 +0300

    code working on Linux

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import os, sys, pickle, signal
 from kivy.config import Config
 Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '600')
@@ -11,8 +11,6 @@ from kivymd.app import MDApp
 from pynput import keyboard
 from pynput.keyboard import Key, Controller
 from threading import Thread
-import pyperclip
-
 
 # Le combinaison a voir
 COMBINATION = {keyboard.Key.cmd, keyboard.Key.shift}
@@ -20,36 +18,35 @@ COMBINATION = {keyboard.Key.cmd, keyboard.Key.shift}
 current = set()
 clavier = Controller()
 _listener = None
+_ecoute = ""
+
 
 
 class Ecoute(Thread):
-	def run(self):
-		with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-			global _listener
-			_listener = listener
-			listener.join()
+    def __init__(self):
+        Thread.__init__(self)
 
 
-def on_press(key):
-    if key in COMBINATION:
-        current.add(key)
-        if all(k in current for k in COMBINATION):
-            print('All modifiers active!')
-            pyperclip.paste()
+    def run(self):
+        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as self.listener:
+            self.listener.join()
 
 
-def on_release(key):
-    try:
-        current.remove(key)
-    except KeyError:
-        pass
+    def on_press(self, key):
+        if key in COMBINATION:
+            current.add(key)
+            if all(k in current for k in COMBINATION):
+                print('All modifiers active!')
+                global _ecoute
+                clavier.type(_ecoute)
 
 
-ecoute = Ecoute()
-ecoute.start()
+    def on_release(self, key):
+        try:
+            current.remove(key)
+        except KeyError:
+            pass
 
-
-_ecoute = ""
 
 
 class PersistentPASS(MDApp):
@@ -58,19 +55,40 @@ class PersistentPASS(MDApp):
     	with open('gui.kv', encoding='utf-8') as f:
             self.GUI = Builder.load_string(f.read())
 
+
     def build(self):
     	self.theme_cls.primary_palette = "Teal"
     	return self.GUI
 
 
     def save(self):
-    	pyperclip.copy(self.GUI.ids.paste.text)
+        global _ecoute
+        _ecoute = self.GUI.ids.paste.text
 
 
     def on_request_close(self, *args):
-    	if _listener:
-    		_listener.stop()
-    	sys.exit(1)
+        ecoute.listener.stop()
+        sys.exit(1)
 
 
-PersistentPASS().run()
+
+def getPid():
+    if os.path.isfile('__pid'):
+        with open('__pid', 'rb') as _pid:
+            pid = pickle.load(_pid)
+        os.kill(pid, signal.SIGSTOP)
+
+
+def setPid():
+    with open('__pid', 'wb') as _pid:
+        pickle.dump(os.getpid(), _pid)
+
+
+
+
+if __name__ == '__main__':
+    getPid()
+    setPid()
+    ecoute = Ecoute()
+    ecoute.start()
+    PersistentPASS().run()
