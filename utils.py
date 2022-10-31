09a41fc6a1912f5189f22b81f42e860c70e1a925
rootkit7628 <arleme.dev7@gmail.com>
2022-10-31 10:57:47 +0300

    [FIX][IMP] Fix MDtoolBar and traduction feature

--- utils.py
+++ utils.py
@@ -1,20 +1,15 @@
-import json 
+import json
+
 
 def translate(key, lang='fr'):
-	"""
-        Une fonction de traduction
     """
-	with open('langs.json') as fichier: 
-		trans = json.load(fichier)
-	mot_cle = trans.get(key)
-	if mot_cle:
-		return mot_cle.get(lang) \
-			if mot_cle.get(lang) else mot_cle
-	else:
-		return key  
-def concatenate(list):
-	res = ""
-	for a in list :
-		res += str(a) + " "
-	return res 
-lang = "en"
+    Une fonction de traduction
+    """
+    with open('langs.json') as fichier:
+        trans = json.load(fichier)
+    mot_cle = trans.get(key)
+    if mot_cle:
+        return mot_cle.get(lang) \
+            if mot_cle.get(lang) else mot_cle
+    else:
+        return key
